bfb83801955abdf12c0cca94c5c23dbab5639356
schiz0id <schiz0id@elite-hackers.com>
2016-06-11 09:04:51 -0500

    Step 2: be organized (add sample test)

---
+++ test/arrayTest.js
@@ -0,0 +1,11 @@
+var chai = require('chai');
+var assert = chai.assert;
+
+describe('Array', function() {
+  it('should start empty', function() {
+    var arr = [];
+    assert.equal(arr.length, 0);
+  });
+
+  // We can have more its here
+});
